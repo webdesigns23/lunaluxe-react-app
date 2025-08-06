b99d5c7e9e8a965860856d167f299c87c0c98986
Sharmaine Perea <sharmaine.perea23@gmail.com>
2025-08-05 23:17:15 -0600

    Destination component added api fetch and Destination Gallery compoinent photos

--- src/components/DestinationGallery.jsx
+++ src/components/DestinationGallery.jsx
@@ -1,11 +1,18 @@
-import React from "react"
+import React from "react";
 
-export default function DestinationGallery() {
+import '../styles/DestinationGallery.css'
+
+export default function DestinationGallery({planets}) {
   return (
-    <>
-      <h1>Scout out some stellar cosmic sanctuaries</h1>
-      
-    </>
+    <div className="gallery">
+      {planets.map(planet => (
+        <img key={planet.id} 
+        src={`src/assets/planets/${planet.id}.jpg`}
+        alt={planet.englishName}
+        className="planet-img"
+        onClick={() => onSelect(planet.id)} />
+      )
+      )};
+    </div>
   );
-}
-
+} 
--- src/pages/Destinations.jsx
+++ src/pages/Destinations.jsx
@@ -1,13 +1,44 @@
-import React from "react";
-import { Outlet, useEffect } from "react-router-dom";
-import DestinationGallery from "../components/DestinationGallery";
-import DestinationInfo from "../components/DestinationInfo";
+import { useState, useEffect } from 'react';
+import DestinationGallery from '../components/DestinationGallery';
+
 
 export default function Destinations() {
+
+  const [planets, setPlanets] = useState([]);
+  const [selectedPlanet, setSelectedPlanet] = useState(null);
+  const [loading, setLoading] = useState(false);
+
+  useEffect(() => {
+    fetch("https://api.le-systeme-solaire.net/rest.php/bodies?filter[]=isPlanet,eq,true")
+      .then(response => {
+        if(response.ok) {
+          return response.json()
+        } else {
+          console.log("fetch request failed")
+        }
+      })
+      .then(data => setPlanets(data.bodies))
+      .catch(error => console.error("Error loading planets", error));
+  }, []);
+
+  const handleSelect = (id) => {
+    setLoading(true);
+    fetch(`https://api.le-systeme-solaire.net/rest/bodies/${id}`)
+      .then(res => res.json())
+      .then(data => {
+        setSelectedPlanet(data);
+        setLoading(false);
+      })
+      .catch(error => {
+        console.error("Error loading planet info", error);
+        setLoading(false);
+      });
+  };
+
   return (
     <div>
-      <h1>Scout out some stellar cosmic sanctuaries</h1>
-      <DestinationGallery />
+      <h1>Scout Out Some Stellar Cosmic Sanctuaries</h1>
+      <DestinationGallery planets={planets} handleSelect={handleSelect}/>
       <Outlet /> {/*to pass state/context to nested component*/}
     </div>
   );
