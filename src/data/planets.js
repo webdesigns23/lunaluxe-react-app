1ed9af5e985eb382a937dd0840c2070e01cc2bb5
Sharmaine Perea <sharmaine.perea23@gmail.com>
2025-08-06 09:59:47 -0600

    added planet data for destination carousel

---
+++ src/data/planets.js
@@ -0,0 +1,48 @@
+const planetCarousel = [
+// {
+//     name: "Moon",
+//     embedUrl: "https://sketchfab.com/3d-models/the-moon-sharp-1bc61d056d934523a3de63d713ce3377/embed",
+//   },
+
+	{
+    name: "Sun",
+    embedUrl: "https://sketchfab.com/3d-models/the-sun-0d28aa65eb174d948c2716d73e8fd3bd/embed",
+  },
+	{
+    name: "Mercury",
+    embedUrl: "https://sketchfab.com/3d-models/mercury-enhanced-color-5931d130a5204419b3323dd41f3506b1/embed",
+  },
+   {
+    name: "Venus",
+    embedUrl: "https://sketchfab.com/3d-models/venus-b306aaadbf2b4fcea1afa2db5ed75b4f/embed",
+  },
+  {
+    name: "Earth",
+    embedUrl: "https://sketchfab.com/3d-models/earth-63d902b12fd14868b4dc2f19dc21d7c2/embed",
+  },
+  {
+    name: "Mars",
+    embedUrl: "https://sketchfab.com/3d-models/mars-7b14d8d8a5b94626a4b2a8a4455297d6/embed",
+  },
+  {
+    name: "Jupiter",
+    embedUrl: "https://sketchfab.com/3d-models/jupiter-c5275eb96af245e4a8453837ac728a62/embed",
+  },
+   {
+    name: "Saturn",
+    embedUrl: "https://sketchfab.com/3d-models/saturno-v11-5d546029721640a9ac332fb68b959e49/embed",
+  },
+   {
+    name: "Uranus",
+    embedUrl: "https://sketchfab.com/3d-models/uranus-b488282fa28344009fa8c8cdb3384456/embed",
+  },
+   {
+    name: "Neptune",
+    embedUrl: "https://sketchfab.com/3d-models/neptune-8c6dc96f47ab4d798a1cb2d827da3fbe/embed",
+  },
+//   {
+//     name: "Pluto",
+//     embedUrl: "https://sketchfab.com/3d-models/pluto-0840325e536d47bdb6ed4b867d55b5c1/embed",
+//   },
+  
+];
