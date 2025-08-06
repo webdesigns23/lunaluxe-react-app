d851fec394600d20a14547f924ce8ab1d926be4b
Sharmaine Perea <sharmaine.perea23@gmail.com>
2025-08-06 13:13:16 -0600

    added rough css and api data to fetch button

--- src/components/DestinationCarousel.jsx
+++ src/components/DestinationCarousel.jsx
@@ -1,5 +1,6 @@
 import React, { useState } from "react";
 import Slider from "react-slick";
+import "../styles/DestinationCarousel.css"
 
 // destination planet data
 import destinations from "../data/destinations";
@@ -11,23 +12,25 @@ export default function DestinationCarousel() {
     speed: 500,
     slidesToShow: 3,
     slidesToScroll: 1,
+	centerMode: true,
   };
-
-  const displayDestinations = destinations.map((destination) => (
-	<div key={destination.name}>
-		<iframe 
-			title={destination.name} 
-			style={{width: "90%", height: "500px"}} 
-			src={destination.embedUrl} alt={destination.alt}>
-		</iframe>
-		<h2>{destination.name}</h2>
-	</div>
-  ))
 
 
   return (
 	<Slider {...settings}>
-		{displayDestinations}  
+		{destinations.map((destination) => (
+		<div className="main-slide" key={destination.name}>
+			<div className="inner-slide">
+				<iframe className="card-frame"
+					title={destination.name} 
+					style={{width: "90%", height: "400px"}} 
+					src={destination.embedUrl} alt={destination.alt}>
+				</iframe>
+				<h2 className="destination-name"> {destination.name}</h2>
+				<button>Start Exploring</button>
+			</div>
+		</div>
+		))}
     </Slider>
   );
 }
