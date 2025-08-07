a4b078c6a93bb63eb139efb6fabe982578034a7e
Sharmaine Perea <sharmaine.perea23@gmail.com>
2025-08-07 16:12:36 -0600

    wrap the app in TripProvider

--- src/main.jsx
+++ src/main.jsx
@@ -2,9 +2,13 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import App from './App.jsx'
 import './index.css'
+import { TripProvider } from './components/TripContext.jsx' //import provider
+
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <App />
+    <TripProvider> {/* wrap your app */}
+      <App />
+    </TripProvider>
   </StrictMode>,
 )
