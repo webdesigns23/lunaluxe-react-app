bbbc897d2e6823206fc472b9aa1649817307b8db
Sharmaine Perea <sharmaine.perea23@gmail.com>
2025-08-07 14:32:56 -0600

    fetch all data for destinations and vessels for user to plan trip on expeditionPlanner page

--- src/components/ExpPlannerForm.jsx
+++ src/components/ExpPlannerForm.jsx
@@ -1,19 +1,9 @@
-import React, { useState } from "react";
+import React from "react";
 
 export default function ExpPlannerForm() {
-  const [destination, setDestination] = useState("");
-  const [vessel, setVessel] = useState("");
-  const [travelTime, setTravelTime] = useState(null);
-  const origin = "sun"
-  
-
-
   return (
     <>
       <h1>Plan Expedition:</h1>
-      <form className="New Expedition">
-        input type 
-      </form>
     </>
   );
 }
--- src/pages/ExpPlanner.jsx
+++ src/pages/ExpPlanner.jsx
@@ -1,6 +1,45 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
+import destinations from "../data/destinations";
+import vessels from "../data/vessels";
 
 export default function ExpPlanner() {
+
+  const [ allDestinations, setAllDestinations] = useState([])
+  const [ allVessles, setAllVessels] = useState([])
+  const [ launching, setlaunching] = useState(true)
+
+  //All Body info
+  useEffect(() => {
+    fetch("https://api.le-systeme-solaire.net/rest.php/bodies?filter[]=isPlanet,eq,true")
+      .then(response => {
+        if(response.ok) {
+          return response.json()
+        } else {
+          console.log("fetch request failed")
+        }
+      })
+      .then(data => setAllDestinations(data.bodies))
+      .catch(error => console.error("Error launching destinations", error));
+  }, []);
+
+
+  //All Vessel info
+  useEffect(() => {
+    fetch("https://swapi.info/api/starships")
+      .then(response => {
+        if(response.ok) {
+          return response.json()
+        } else {
+          console.log("fetch request failed")
+        }
+      })
+      .then(data => setAllVessels(data.bodies))
+      .catch(error => console.error("Error launching space vessels", error));
+  }, []);
+
+
+   if (launching) return <p>Launching your next adventure...</p>;
+
   
   return (
     <header>
