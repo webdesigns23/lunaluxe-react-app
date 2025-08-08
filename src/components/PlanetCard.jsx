4b22e8d5d90e97797b068c50a4f05026e5f7d6f8
Sharmaine Perea <sharmaine.perea23@gmail.com>
2025-08-08 10:11:24 -0600

    created planetcard and planetlist components

--- src/components/PlanetCard.jsx
+++
@@ -1,8 +0,0 @@
-import React from "react";
-
-export default PlanetCard() {
-
-	return(
-		<div></div>
-	)
-}
--- src/components/PlanetList.jsx
+++
@@ -1,8 +0,0 @@
-import React from "react";
-
-export default PlanetList() {
-
-	return(
-		<div></div>
-	)
-}
